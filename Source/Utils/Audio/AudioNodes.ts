de43f4152f027fde1e8fcbe5ddefb69933b2ce51
Venryx <venryx@gmail.com>
2019-08-14 05:51:24 -0700

    * Standardized intensity/loudness to loudness.

--- Source/Utils/Audio/AudioNodes.ts
+++ Source/Utils/Audio/AudioNodes.ts
@@ -12,24 +12,24 @@ const GeneralAudioProcessor_code = `
 			this.port.onmessage = event=> {
 				let message = event.data;
 				if (message.type == "init") {
-					this.notifyIntensities = message.notifyIntensities;
+					this.notifyLoudnesses = message.notifyLoudnesses;
 					this.minNotifyInterval = message.minNotifyInterval;
-					this.logIntensities = message.logIntensities;
+					this.logLoudnesses = message.logLoudnesses;
 				}
 			};
 			//this.port.start();
 
 			// must initialize in constructor, to work in electron
-			this.notifyIntensities = [];
-			this.notifyIntensities_lastTimes = [];
+			this.notifyLoudnesses = [];
+			this.notifyLoudnesses_lastTimes = [];
 			this.minNotifyInterval = 0;
 			this.lastLogTime = 0;
 		}
 		
-		/*notifyIntensities = [];
-		notifyIntensities_lastTimes = [];
+		/*notifyLoudnesses = [];
+		notifyLoudnesses_lastTimes = [];
 		minNotifyInterval = 0;
-		logIntensities = false;
+		logLoudnesses = false;
 
 		lastLogTime = 0;*/
 		FrequentLog(str) {
@@ -47,16 +47,17 @@ const GeneralAudioProcessor_code = `
 					// we don't actually want to output any sound, we're just listening; so don't transfer input-data to output-data
 					//channel[i] = inputChannels[index][i];
 
-					let actualIntensity = inputChannels[index][i];
-					if (this.logIntensities) {
-						this.FrequentLog("Actual intensity: " + actualIntensity);
+					let actualLoudness = inputChannels[index][i];
+					//let db = actualLoudness == 0 ? 0 : 20.0 * Math.log10(actualLoudness) + 90
+					if (this.logLoudnesses) {
+						this.FrequentLog("Actual loudness: " + actualLoudness);
 					}
-					for (let [index, notifyIntensity] of this.notifyIntensities.entries()) {
-						if (actualIntensity >= notifyIntensity) {
-							let lastTime = this.notifyIntensities_lastTimes[index] || 0;
+					for (let [index, notifyLoudness] of this.notifyLoudnesses.entries()) {
+						if (actualLoudness >= notifyLoudness) {
+							let lastTime = this.notifyLoudnesses_lastTimes[index] || 0;
 							if (Date.now() >= lastTime + this.minNotifyInterval) {
-								this.port.postMessage({type: "notify-intensity", notifyIntensity, actualIntensity});
-								this.notifyIntensities_lastTimes[index] = Date.now();
+								this.port.postMessage({type: "notify-loudness", notifyLoudness, actualLoudness});
+								this.notifyLoudnesses_lastTimes[index] = Date.now();
 							}
 						}
 					}
