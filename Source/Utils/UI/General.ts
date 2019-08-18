94db6ba27f095a40a8e44ac7c2910859e8267c92
Venryx <venryx@gmail.com>
2019-08-17 20:26:15 -0700

    * Added HSL and HSLA funcs. (same as CSSColor except with css-str-type prespecified)

--- Source/Utils/UI/General.ts
+++ Source/Utils/UI/General.ts
@@ -7,19 +7,26 @@ export function StandardCompProps() {
 
 export type CSSColorStringType = "hsl" | "hsla";
 /** Converts color-props into a css color-string of the specified format. */
-export function Color(
+export function CSSColor(
 	/** [0-360] RefPoints: {red: 0, orange: 38, yellow: 60, green: 120, blue: 240, violet: 300, indigo: 274[?]} */
 	hue: number,
-	/** [0-1] */
-	saturation: number,
-	/** [0-1] */
-	brightness: number,
-	/** [0-1] */
-	alpha = 1,
+	/** [0-1] */ saturation: number, /** [0-1] */ brightness: number, /** [0-1] */ alpha = 1,
 	cssType: CSSColorStringType = "hsla",
 ) {
 	if (cssType == "hsl") return `hsl(${hue}, ${saturation * 100}%, ${brightness * 100}%)`;
 	if (cssType == "hsla") return `hsla(${hue}, ${saturation * 100}%, ${brightness * 100}%, ${alpha})`;
+}
+export function HSL(
+	/** [0-360] RefPoints: {red: 0, orange: 38, yellow: 60, green: 120, blue: 240, violet: 300, indigo: 274[?]} */ hue: number,
+	/** [0-1] */ saturation: number, /** [0-1] */ brightness: number,
+) {
+	return CSSColor(hue, saturation, brightness, null, "hsl");
+}
+export function HSLA(
+	/** [0-360] RefPoints: {red: 0, orange: 38, yellow: 60, green: 120, blue: 240, violet: 300, indigo: 274[?]} */ hue: number,
+	/** [0-1] */ saturation: number, /** [0-1] */ brightness: number, /** [0-1] */ alpha = 1,
+) {
+	return CSSColor(hue, saturation, brightness, alpha, "hsla");
 }
 
 export function ElementAcceptsTextInput(element: Element) {
