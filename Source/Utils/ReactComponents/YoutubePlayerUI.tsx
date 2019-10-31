849906b6383d16721787e14368a6d419ebd3a716
Venryx <venryx@gmail.com>
2019-10-30 18:16:22 -0700

    * Added handy UseSize React hook. (see CD for usage example)

--- Source/Utils/ReactComponents/YoutubePlayerUI.tsx
+++ Source/Utils/ReactComponents/YoutubePlayerUI.tsx
@@ -1,17 +1,21 @@
-import React from "react";
-import {BaseComponent} from "react-vextensions";
+import React, {Ref} from "react";
+import {BaseComponent, BaseComponentPlus} from "react-vextensions";
 import {Row} from "react-vcomponents";
 import {YoutubePlayer} from "../General/YoutubePlayer";
 
-export class YoutubePlayerUI extends BaseComponent<{videoID: string, startTime?: number, heightVSWidthPercent: number, autoplay: boolean, initPlayer?: (player: YoutubePlayer)=>any, onPlayerInitialized?: (player: YoutubePlayer)=>any}, {}> {
-	static defaultProps = {heightVSWidthPercent: .75, autoplay: false};
-
+export class YoutubePlayerUI extends BaseComponentPlus(
+	{heightVSWidthPercent: .75, autoplay: false} as {
+		videoID: string, startTime?: number, heightVSWidthPercent: number, autoplay?: boolean,
+		initPlayer?: (player: YoutubePlayer)=>any, onPlayerInitialized?: (player: YoutubePlayer)=>any,
+		style?,
+	}, {},
+) {
 	player: YoutubePlayer;
 	root: HTMLDivElement;
 	render() {
-		const {heightVSWidthPercent} = this.props;
+		const {heightVSWidthPercent, style} = this.props;
 		return (
-			<div ref={c=>this.root = c} style={{position: "relative", paddingBottom: `${heightVSWidthPercent * 100}%`, height: 0}}>
+			<div ref={c=>this.root = c} style={E({position: "relative", paddingBottom: `${heightVSWidthPercent * 100}%`, height: 0}, style)}>
 			</div>
 		);
 	}
--- Source/Utils/UI/ReactHooks.tsx
+++ Source/Utils/UI/ReactHooks.tsx
@@ -1,3 +1,8 @@
+import {UseState} from "react-vextensions";
+import {VRect, ToJSON} from "js-vextensions";
+import {useRef, useLayoutEffect, MutableRefObject, useState, useCallback} from "react";
+import {shallowEqual} from "react-redux";
+
 // general
 // ==========
 
@@ -25,3 +30,51 @@
 	}, []);
 	return checkStillHoveredTimer;
 } */
+
+export type Size = {width: number, height: number};
+export enum UseSize_Method {
+	/** How much of the parent's "relative positioning" space is taken up by the element. (ie. it ignores the element's position: absolute descendents) */
+	OffsetSize = 10,
+	/** Same as OffsetSize, except it excludes the element's own border, margin, and the height of its horizontal scroll-bar (if it has one). */
+	ClientSize = 20,
+	/** How much space is needed to see all of the element's content/descendents (including position: absolute ones) without scrolling. */
+	ScrollSize = 30,
+	/** Same as ScrollSize, except that it's calculated after the element's css transforms are applied. */
+	BoundingClientRect = 40,
+}
+export class UseSize_Options {
+	method = UseSize_Method.OffsetSize;
+}
+export function GetSize(el: HTMLElement, method: UseSize_Method) {
+	let size: Size;
+	if (method == UseSize_Method.OffsetSize) {
+		size = {width: el.offsetWidth, height: el.offsetHeight};
+	} else if (method == UseSize_Method.ClientSize) {
+		size = {width: el.clientWidth, height: el.clientHeight};
+	} else if (method == UseSize_Method.ScrollSize) {
+		size = {width: el.scrollWidth, height: el.scrollHeight};
+	} else if (method == UseSize_Method.BoundingClientRect) {
+		size = el.getBoundingClientRect().Including("width", "height") as any;
+	}
+	return size;
+}
+export function UseSize(options?: Partial<UseSize_Options>): [(node: Element)=>any, Size] {
+	options = E(new UseSize_Options(), options);
+	const [size, setSize] = UseState({width: null, height: null} as Size, shallowEqual);
+
+	//const ref = useRef<HTMLElement>();
+	const [node, setNode] = UseState(null);
+	const ref = useCallback(newNode=>{
+		setNode(newNode);
+	}, []);
+
+	useLayoutEffect(()=>{
+		if (node == null) return;
+		window.requestAnimationFrame(()=>{
+			//const el = ref.current as HTMLElement;
+			const newSize = GetSize(node, options.method);
+			setSize(newSize);
+		});
+	}, [node]);
+	return [ref, size];
+}
