d80394750ea2cb37cbd81e8c6f89df8484be7091
Venryx <venryx@gmail.com>
2019-12-07 07:34:59 -0800

    *

--- Source/index.ts
+++ Source/index.ts
@@ -8,7 +8,7 @@ import * as frameworkExportsObject from ".";
 
 //import "js-vextensions/Source/ClassExtensions/@ApplyTypes";
 type __ = typeof import("../node_modules/js-vextensions/Helpers/@ApplyCETypes");
-import "js-vextensions/Helpers/@ApplyCECode"; // eslint-disable-line
+import "js-vextensions/Helpers/@ApplyCECode.js"; // eslint-disable-line
 
 export * from "./@EnableExportOverwrites";
 // special key, marking this module's exports object, which allows the patch in ExportOverwriteEnabler.ts to make the remaining exports overwriteable
