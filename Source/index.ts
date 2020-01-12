792e8c42119bd84c81d93982547ef3cbf9e831fd
Venryx <venryx@gmail.com>
2020-01-11 20:11:04 -0800

    * Moved KeyGenerator to vwebapp-framework. * MS GenerateUUID, by default, avoids a few "unpleasant character-combinations" for the first two characters. (since we display the first 2 chars of UUIDs in various places)

--- Source/index.ts
+++ Source/index.ts
@@ -31,6 +31,7 @@ export * from "./Utils/General/Constants";
 export * from "./Utils/General/Errors";
 export * from "./Utils/General/General";
 export * from "./Utils/General/Geometry";
+export * from "./Utils/General/KeyGenerator";
 export * from "./Utils/General/KeyNames";
 export * from "./Utils/General/Logging";
 export * from "./Utils/General/ModuleExportExposer";
